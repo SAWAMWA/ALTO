76f19cfb6b29204742228187ed9a431862966ced
SAWAMWA <smlmwangi@gmail.com>
2020-09-29 20:49:50 +0300

    coomit

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,11 +8,11 @@ export class Footer extends Component {
             <div>
                 <Card bg="primary" expand="lg" >
                 <Card.Body>
-                    <Card.Title>Allto Construction Company ltd</Card.Title>
-                    <Card.Text>
-                        P.O BOX 50221 -00100 NAIROBI: 
-                        TELL:  +254 724 59 81 40 
-                        OFFICE: +254 736 80 01 59
+                    <Card.Title tyle={{color:"#fff"}} >Allto Construction Company ltd</Card.Title>
+                    <Card.Text style={{color:"#fff"}}>
+                        P.O BOX 50221 -00100 NAIROBI | 
+                        TELL:  +254 724 59 81 40    |
+                        OFFICE: +254 736 80 01 59 |
                         Email:
                     </Card.Text>
                 </Card.Body>
